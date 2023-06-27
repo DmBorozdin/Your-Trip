3f48f47c5f3a360a7044bc6d7f73192987f41fc4
Dmitrii Borozdin <borozdin1@yandex.ru>
2023-06-27 21:29:06 +0700

    Created app with CRA. Install react-router-dom

---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  console.log('Hello');
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
