be113fb7d161fae33fe6c35e06cc37f6af1a75fb
Dmitrii Borozdin <borozdin1@yandex.ru>
2023-07-05 10:38:00 +0700

    deleted useless fragment

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -50,85 +50,83 @@ const mockData = [
 
 const Main = () => {
   return (
-    <React.Fragment>
-      <main className={styles.main}>
-        <div>
-          <section className={styles.container}>
-            <ul className={styles.locationsList}>
-              <li className={styles.locationsItem}>
-                <a className={styles.locationsItemLink} href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className={styles.locationsItem}>
-                <a className={styles.locationsItemLink} href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className={styles.locationsItem}>
-                <a className={styles.locationsItemLink} href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className={styles.locationsItem}>
-                <a
-                  className={`${styles.locationsItemLink} ${styles.locationsItemLinkActive}`}
+    <main className={styles.main}>
+      <div>
+        <section className={styles.container}>
+          <ul className={styles.locationsList}>
+            <li className={styles.locationsItem}>
+              <a className={styles.locationsItemLink} href="#">
+                <span>Paris</span>
+              </a>
+            </li>
+            <li className={styles.locationsItem}>
+              <a className={styles.locationsItemLink} href="#">
+                <span>Cologne</span>
+              </a>
+            </li>
+            <li className={styles.locationsItem}>
+              <a className={styles.locationsItemLink} href="#">
+                <span>Brussels</span>
+              </a>
+            </li>
+            <li className={styles.locationsItem}>
+              <a
+                className={`${styles.locationsItemLink} ${styles.locationsItemLinkActive}`}
+              >
+                <span>Amsterdam</span>
+              </a>
+            </li>
+            <li className={styles.locationsItem}>
+              <a className={styles.locationsItemLink} href="#">
+                <span>Hamburg</span>
+              </a>
+            </li>
+            <li className={styles.locationsItem}>
+              <a className={styles.locationsItemLink} href="#">
+                <span>Dusseldorf</span>
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <div className={styles.cities}>
+        <div className={`${styles.placesContainer} ${styles.container}`}>
+          <section className={styles.places}>
+            <b className={styles.placesFound}>
+              312 places to stay in Amsterdam
+            </b>
+            <form className={styles.sorting} action="#" method="get">
+              <span className={styles.sortingCaption}>Sort by</span>
+              <span className={styles.sortingType} tabIndex={0}>
+                Popular
+                <ExpandMore className={styles.sortingArrow} />
+              </span>
+              <ul className={styles.options}>
+                <li
+                  className={`${styles.option} ${styles.optionActive}`}
+                  tabIndex={0}
                 >
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className={styles.locationsItem}>
-                <a className={styles.locationsItemLink} href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className={styles.locationsItem}>
-                <a className={styles.locationsItemLink} href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
-        <div className={styles.cities}>
-          <div className={`${styles.placesContainer} ${styles.container}`}>
-            <section className={styles.places}>
-              <b className={styles.placesFound}>
-                312 places to stay in Amsterdam
-              </b>
-              <form className={styles.sorting} action="#" method="get">
-                <span className={styles.sortingCaption}>Sort by</span>
-                <span className={styles.sortingType} tabIndex={0}>
                   Popular
-                  <ExpandMore className={styles.sortingArrow} />
-                </span>
-                <ul className={styles.options}>
-                  <li
-                    className={`${styles.option} ${styles.optionActive}`}
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className={`${styles.option}`} tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className={`${styles.option}`} tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className={`${styles.option}`} tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <CardsList rooms={mockData} />
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
+                </li>
+                <li className={`${styles.option}`} tabIndex={0}>
+                  Price: low to high
+                </li>
+                <li className={`${styles.option}`} tabIndex={0}>
+                  Price: high to low
+                </li>
+                <li className={`${styles.option}`} tabIndex={0}>
+                  Top rated first
+                </li>
+              </ul>
+            </form>
+            <CardsList rooms={mockData} />
+          </section>
+          <div className="cities__right-section">
+            <section className="cities__map map"></section>
           </div>
         </div>
-      </main>
-    </React.Fragment>
+      </div>
+    </main>
   );
 };
 
