0e1362632c66d7f99fccd90dba8b2a24c10e36ae
Dmitrii Borozdin <borozdin1@yandex.ru>
2023-07-14 18:50:39 +0700

    Added use of ErrorBoundary. Created ErrorFallback component. Added styles for NotFoundScreen component

--- src/components/not-found-screen/not-found-screen.tsx
+++ src/components/not-found-screen/not-found-screen.tsx
@@ -1,17 +1,13 @@
-import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { APPRoute } from "../../const";
+import styles from "./not-found-screen.module.scss";
 
 const NotFoundScreen = () => {
   return (
-    <Fragment>
-      <div className="user-page">
-        <div className="user-page__content">
-          <h1>404. Page not found</h1>
-          <Link to={APPRoute.MAIN}>Go to the home page</Link>
-        </div>
-      </div>
-    </Fragment>
+    <main className={styles.container}>
+      <h1>404. Page not found</h1>
+      <Link to={APPRoute.MAIN}>Go to the home page</Link>
+    </main>
   );
 };
 
