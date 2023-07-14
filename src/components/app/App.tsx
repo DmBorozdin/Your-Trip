d642dc21c87f871def04283ca7471c24e89fecf5
Dmitrii Borozdin <borozdin1@yandex.ru>
2023-07-14 17:40:00 +0700

    Added use of lazy + Suspense

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,33 +1,39 @@
-import React from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Main from "../main/main";
-import Login from "../login/login";
-import SignUp from "../sign-up/sign-up";
-import Favorites from "../favorites/favorites";
-import Room from "../room/room";
-import NotFoundScreen from "../not-found-screen/not-found-screen";
-import Header from "../header/header";
 import ProtectedRoute from "../protected-route/protected-route";
 import { APPRoute } from "../../const";
 import { useSelector } from "react-redux";
 import { getUserData } from "../../store/users/selector";
+import { Skeleton, Spin } from "antd";
+
+const Header = lazy(() => import("../header/header"));
+const Main = lazy(() => import("../main/main"));
+const Login = lazy(() => import("../login/login"));
+const SignUp = lazy(() => import("../sign-up/sign-up"));
+const Favorites = lazy(() => import("../favorites/favorites"));
+const Room = lazy(() => import("../room/room"));
+const NotFoundScreen = lazy(
+  () => import("../not-found-screen/not-found-screen")
+);
 
 const App = () => {
   const { authUser } = useSelector(getUserData);
 
   return (
-    <Routes>
-      <Route element={<Header />}>
-        <Route path={APPRoute.MAIN} element={<Main />} />
-        <Route path={APPRoute.LOGIN} element={<Login />} />
-        <Route path={APPRoute.SIGNUP} element={<SignUp />} />
-        <Route element={<ProtectedRoute authUser={authUser} />}>
-          <Route path={APPRoute.FAVORITES} element={<Favorites />} />
+    <Suspense fallback={<Skeleton active />}>
+      <Routes>
+        <Route element={<Header />}>
+          <Route path={APPRoute.MAIN} element={<Main />} />
+          <Route path={APPRoute.LOGIN} element={<Login />} />
+          <Route path={APPRoute.SIGNUP} element={<SignUp />} />
+          <Route element={<ProtectedRoute authUser={authUser} />}>
+            <Route path={APPRoute.FAVORITES} element={<Favorites />} />
+          </Route>
+          <Route path={APPRoute.ROOM + APPRoute.ID} element={<Room />} />
         </Route>
-        <Route path={APPRoute.ROOM + APPRoute.ID} element={<Room />} />
-      </Route>
-      <Route path={APPRoute.NOTFOUND} element={<NotFoundScreen />} />
-    </Routes>
+        <Route path={APPRoute.NOTFOUND} element={<NotFoundScreen />} />
+      </Routes>
+    </Suspense>
   );
 };
 
